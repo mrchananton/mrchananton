2ef0f54581eb44801a747330cbb32a43454a5e5c
VladimirPal <mail.vpal@gmail.com>
2021-04-01 12:48:09 +0300

    babel config

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  console.log(req.someTestKey ? 'Yo' : 'Optional chaining');
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 
